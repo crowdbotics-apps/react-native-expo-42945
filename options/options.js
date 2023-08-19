88d7fb06e21a04ff0c81e1fb35fed5be1fd11a97
Crowdbotics <team@crowdbotics.com>
2023-08-19 09:07:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "react-native-expo-42945",
+  url: "https://react-native-expo-42945.botics.co",
+  api: "https://react-native-expo-42945.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
